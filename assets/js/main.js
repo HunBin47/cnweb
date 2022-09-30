6d3460aee2f0255c4fe436b548ac1e683fd370ee
HunBin47 <nhathung12042002@gmail.com>
2022-09-30 15:31:38 +0700

    update 30 9 2022

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// window.onscroll = function() {scrollingNavbar()};
+// var navi = document.getElementsByClassName("header__navigation");
+// var naviMiniHeight = navi.offsetTop;
+// function scrollingNavbar() {
+//   if (window.pageYOffset >= naviMiniHeight) {
+//     navi.classList.add("naviMiniHeight")
+//   } else {
+//     navi.classList.remove("naviMiniHeight");
+//   }
+// }
+
+
+
+
+// const navbar = document.getElementsByClassName('wrapper__search');
+// let top = navbar.offsetTop;
+// function stickynavbar() {
+//   if (window.scrollY >= top) {    
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');    
+//   }
+// }
+// window.addEventListener('scroll', stickynavbar);
